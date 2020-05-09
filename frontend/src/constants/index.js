281af99b8fa4b0f1c084eb29f29feebbcf457391
Abiola Adebanjo <abiola.adebanjo@andela.com>
2020-05-09 17:56:04 +0100

    Heroku port fix

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,3 +1,5 @@
 const serverPort = process.env.PORT
+console.log("SERVER_PORT: " + serverPort)
 export const API_BASE_URL = serverPort ? `http://localhost:${serverPort}/api/v1` : process.env.API_BASE_URL || 'http://localhost:5000/api/v1';
+console.log("API_BASE_URL: " + API_BASE_URL)
 export const ACCESS_TOKEN = 'access_token';
